551fe0d27cf14b81eed66138248ad39b279432aa
umutcanaltin342 <136065710+umutcanaltin342@users.noreply.github.com>
2023-06-09 11:11:42 +0300

    Add files via upload

---
+++ muzikcalar.ino
@@ -0,0 +1,26 @@
+#include <SimpleSDAudio.h>
+
+void setup() {
+
+  SdPlay.setSDCSPin(4);  // sd kart cs pini
+
+  if (!SdPlay.init(SSDA_MODE_FULLRATE | SSDA_MODE_MONO | SSDA_MODE_AUTOWORKER)) {
+    while (1)
+      ;
+  }
+
+  if (!SdPlay.setFile("music.wav"))  // müzik dosya adı
+  {
+    while (1)
+      ;  //dosya bulunamassa durdur
+  }
+}
+
+void loop(void) {
+
+  SdPlay.play();  // seçilen dosyayı çal
+
+  while (!SdPlay.isStopped()) {  // müzik bittiğinde tekrar çal
+    ;
+  }
+}
